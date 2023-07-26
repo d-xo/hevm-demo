a22ff4245bde7b5b5c5d611d4fa1cd5185b864f3
dxo <hi@d-xo.org>
2023-07-26 16:31:25 +0200

    add hevm equiv example

--- Makefile
+++ Makefile
@@ -8,3 +8,11 @@ erc20-soltest:
 	hevm test --match "ERC20" --verbose 2
 erc721-soltest:
 	hevm test --match "ERC721A" --verbose 2 --max-iterations 32
+act-equiv:
+	act hevm --spec act/transfer.act --sol act/transfer.sol --contract Token
+act-equiv-bad:
+	act hevm --spec act/transfer.act --sol act/transfer-bad.sol --contract Token
+hevm-equiv:
+	solc act/transfer-bad.sol --bin-runtime -o out/solc/transfer-bad.out --overwrite
+	solc act/transfer.sol --bin-runtime -o out/solc/transfer.out --overwrite
+	hevm equivalence --code-a $$(cat out/solc/transfer.out/Token.bin-runtime) --code-b $$(cat out/solc/transfer-bad.out/Token.bin-runtime)
--- flake.nix
+++ flake.nix
@@ -2,18 +2,22 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hevm.url = "github:ethereum/hevm/default-to-no-ds-test";
+    act.url = "github:ethereum/act";
     flake-utils.url = "github:numtide/flake-utils";
     foundry.url = "github:shazow/foundry.nix";
   };
 
-  outputs = { self, nixpkgs, hevm, flake-utils, foundry, ... }:
+  outputs = { self, nixpkgs, hevm, flake-utils, foundry, act, ... }:
     flake-utils.lib.eachDefaultSystem (system :
      let pkgs = nixpkgs.legacyPackages.${system}; in
        {
          devShell = pkgs.mkShell {
            buildInputs = [
+             act.packages.${system}.default
              hevm.packages.${system}.default
              foundry.defaultPackage.${system}
+             pkgs.solc
+             pkgs.coq
            ];
          };
        });
